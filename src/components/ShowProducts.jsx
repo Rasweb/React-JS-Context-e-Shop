85966925fbca105f4b753bacc17885de2ff295d0
Rasweb <Rasmuspalm@fastmail.se>
2022-07-28 16:30:59 +0200

    Add to cart button on show all products

--- src/components/ShowProducts.jsx
+++ src/components/ShowProducts.jsx
@@ -2,8 +2,25 @@ import { Link } from "react-router-dom";
 import { StyledProductsLi } from "../styledComponents/StyledLi";
 import { StyledProductsImg } from "../styledComponents/StyledImg";
 import { StyledProductsParagraph } from "../styledComponents/StyledParagraph";
+import { CartContext } from "../contexts/CartContext";
+import { useContext } from "react";
 
 export const ShowProducts = ({ product }) => {
+  const [cart, setCart] = useContext(CartContext);
+
+  const addToCart = () => {
+    const tshirt = {
+      id: product.id,
+      name: product.title,
+      price: product.price,
+      image: product.image,
+    };
+    setCart((currCart) => [...currCart, tshirt]);
+    if (cart) {
+      console.log(cart);
+    }
+  };
+
   return (
     <StyledProductsLi key={product.id}>
       <h3>{product.title}</h3>
@@ -15,6 +32,7 @@ export const ShowProducts = ({ product }) => {
       <Link to={`/products/${product.id}`} key={product.id}>
         <button>More info</button>
       </Link>
+      <button onClick={addToCart}>Add to cart</button>
     </StyledProductsLi>
   );
 };
